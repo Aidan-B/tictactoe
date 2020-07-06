89e6e6cbcd536716f0209f5f38d7d0d87892c347
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2020-07-05 21:11:12 -0400

    implemented win checking

--- js/index.js
+++ js/index.js
@@ -3,20 +3,21 @@ $(document).ready(() => {
     //handles player selection
     function selectSquare(element) {
         if (element.is(":empty")) {
-            updateImage(turn, element);
-            turn = !turn;
+            updateImage(player, element);
         }
     }
 
     //Sets the grid image to player token
-    function updateImage(player1, square) {
-        switch (player1) {
-            case true: //player 1 (cross)
+    function updateImage(player, square) {
+        switch (player) {
+            case false: //player 1 (cross)
                 square.html('<img src="./resources/Cross.svg" class="player-token"></img>');
+                square.addClass("cross");
                 break;
 
-            case false: //player 2 (circle)
+            case true: //player 2 (circle)
                 square.html('<img src="./resources/Circle.svg" class="player-token"></img>');
+                square.addClass("circle")
                 break;
         
             default: //error
@@ -25,29 +26,64 @@ $(document).ready(() => {
         }
     }
 
+    //Clears the game board
     function clearGrid() {
-        console.log("clear button");
         $(".game-square").html("");
+        $(".game-square").removeClass("circle");
+        $(".game-square").removeClass("cross");
 
     }
 
-    function checkForWin() {
+    //Checks for a winner and updates the win status
+    function checkForWin(player) {
 
+        //might be a good idea to store this in an array on the server side so that people cant
+        //just go into the inspector to win... although that sounds like a fun cheat code :)
+        let $token = $("#gameGrid").children(".row").find(".game-square");
+        
+        let tokenName = (player ? 'circle' : 'cross');
+
+        //TODO: Maybe if I want to make the board scaleable I can apply some math here,
+        //      I figure this is appropriate for this situation.
+
+                //Win for row, (0,1,2),(3,4,5),(6,7,8)
+        if ($token.eq(0).hasClass(tokenName) && $token.eq(1).hasClass(tokenName) && $token.eq(2).hasClass(tokenName) ||
+            $token.eq(3).hasClass(tokenName) && $token.eq(4).hasClass(tokenName) && $token.eq(5).hasClass(tokenName) ||
+            $token.eq(6).hasClass(tokenName) && $token.eq(7).hasClass(tokenName) && $token.eq(8).hasClass(tokenName)
+            ||  //Win for column (0,3,6),(1,4,7),(2,5,8)
+            $token.eq(0).hasClass(tokenName) && $token.eq(3).hasClass(tokenName) && $token.eq(6).hasClass(tokenName) ||
+            $token.eq(1).hasClass(tokenName) && $token.eq(4).hasClass(tokenName) && $token.eq(7).hasClass(tokenName) ||
+            $token.eq(2).hasClass(tokenName) && $token.eq(5).hasClass(tokenName) && $token.eq(8).hasClass(tokenName)                
+            ||  //Win for diagonals (0,4,8),(2,4,6)
+            $token.eq(0).hasClass(tokenName) && $token.eq(4).hasClass(tokenName) && $token.eq(8).hasClass(tokenName) ||
+            $token.eq(2).hasClass(tokenName) && $token.eq(4).hasClass(tokenName) && $token.eq(6).hasClass(tokenName)
+            ) {
+
+            console.log(tokenName + " win");
+            return true;
+        }
+        return false;   
     }
 
 
-
-    turn = true;
+    //player 0 is cross, player 1 is circle
+    player = false;
     gameOver = false;
     
+    //User clicks a square
     $(".game-square").click( function() {
-        selectSquare($(this));
-        gameOver = checkForWin();
+        if (!gameOver) {
+            selectSquare($(this));
+            gameOver = checkForWin(player);
+            player = !player;
+        }        
     });
 
+    //User clicks reset
     $("#reset-button").click(function() {
         clearGrid();
-        turn = true
+        player = false;
+        gameOver = false;
     });
     
 });
